42ac313245108a9651141e1daa585e5c0ed56c89
Farrah Mae Ochoa <farrah@deriv.com>
2025-04-28 16:35:33 +0400

    fix: word wrap

--- components/buy-sell/order-sidebar.tsx
+++ components/buy-sell/order-sidebar.tsx
@@ -200,7 +200,7 @@ export default function OrderSidebar({ isOpen, onClose, ad, orderType }: OrderSi
             </div>
             <div className="mx-4 mt-4 border-t py-2 text-sm">
               <h3 className="text-gray-500">{isBuy ? "Buyer's instructions" : "Seller's instructions"}</h3>
-              <p className="text-gray-800">
+              <p className="text-gray-800 break-words">
                 {ad.description ||
                   "Kindly transfer the payment to the provided account details after placing your order."}
               </p>
