143c1be4963fa4c9a09d130c5d5c5ad8e9da6861
ernest-deriv <ernest@regentmarkets.com>
2025-05-28 15:31:29 +0400

    update height of header

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   ]
 
   return (
-    <header className="hidden md:block fixed top-0 left-0 right-0 bg-white border-b border-slate-200 z-10">
+    <header className="hidden md:block fixed top-0 left-0 right-0 bg-white border-b border-slate-200 z-10 h-16">
       <div className="container mx-auto px-4">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
